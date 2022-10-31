291908347f7cc0a5dd6a384678b629002f617420
vcatelan <lagoascatelan@gmail.com>
2022-10-31 19:09:40 -0300

    inclusão modal

---
+++ js/formulario.js
@@ -0,0 +1,59 @@
+function Enviar() {
+    var nome = document.getElementById("nomeid");
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+}
+
+function initScrollSuave (){
+
+    const linksInternos = document.querySelectorAll ('.js-menu a[href^="#"]');
+
+        function scrollToSection (event) {
+            event.preventDefault();
+            const href = event.currentTarget.getAttribute('href');
+            const section = document.querySelector(href);
+            section.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            })
+        }
+
+    linksInternos.forEach ((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
+
+}
+
+initScrollSuave ();
+
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll ('.js-scroll');
+    if(sections.length){
+        const windowMetade = window.innerHeight * 0.5;
+
+    function animaScroll (){
+        sections.forEach((section) => {
+            const sectionTop = section.getBoundingClientRect().top;
+            const isSectionVisible = (sectionTop - windowMetade) < 0;
+            if(isSectionVisible)
+                section.classList.add('ativo');
+            else
+                section.classList.remove('ativo');
+        })
+    }
+
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+    }
+}
+
+initAnimacaoScroll ();
+
+var splide = new Splide( '.splide', {
+    type   : 'loop',
+    padding: '5rem',
+  } );
+  
+  splide.mount();
